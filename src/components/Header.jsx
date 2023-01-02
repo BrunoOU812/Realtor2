e1c06cfbc0c8a98e7b5012366544fab858b0d4c0
Bruno Morgades <brunomorgades@hotmail.com>
2023-01-02 16:44:04 -0300

    First commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <header>
+        <div>
+          <img
+            src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg"
+            alt=""
+            className="h-5"
+          />
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
