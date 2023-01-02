67dbd10e38b168f7d4596ddd72d92260910e3739
Bruno Morgades <brunomorgades@hotmail.com>
2023-01-02 16:51:40 -0300

    create Routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
 function App() {
   return (
     <Router>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/Profile" element={<Profile />} />
+        <Route path="/SignIn" element={<SignIn />} />
+        <Route path="/SignUp" element={<SignUp />} />
+        <Route path="/ForgotPassword" element={<ForgotPassword />} />
+        <Route path="/Offers" element={<Offers />} />
       </Routes>
     </Router>
   );
