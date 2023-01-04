3e093b85d74381bb2750fc9429afdba1609e14e4
Bruno Morgades <brunomorgades@hotmail.com>
2023-01-04 10:22:09 -0300

    Create Navbar or Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,50 @@
 import React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 function Header() {
+  const location = useLocation().pathname;
+  const nav = useNavigate();
+  const pathMatches = (route) => {
+    return location == `${route}` && "border-b-red-500 text-black";
+  };
   return (
-    <div>
-      <header>
+    <div className="bg-white border-b shadow-sm sticky top-0 z-50">
+      <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
         <div>
           <img
             src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg"
             alt=""
-            className="h-5"
+            className="h-5 cursor-pointer"
           />
+        </div>
+        <div>
+          <ul className="flex space-x-10">
+            <li
+              onClick={() => nav("/")}
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatches(
+                "/"
+              )}
+              `}
+            >
+              Home
+            </li>
+            <li
+              onClick={() => nav("/Offers")}
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatches(
+                "/Offers"
+              )}`}
+            >
+              Offers
+            </li>
+            <li
+              onClick={() => nav("/SignIn")}
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent  ${pathMatches(
+                "/SignIn"
+              )}`}
+            >
+              Sign in
+            </li>
+          </ul>
         </div>
       </header>
     </div>
